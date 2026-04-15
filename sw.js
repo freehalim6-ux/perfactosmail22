48ab819d888edbe6c636c8dd50cecee73e16f7a6
freehalim6-ux <freehalim6@gmail.com>
2026-04-15 22:35:32 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,3 @@
-const CACHE_NAME = 'perfacto-smile-v1';
-const urlsToCache = ['./', './index.html', './manifest.json'];
-
-self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
+self.addEventListener('fetch', function(event) {
+  // بسيط جداً، فقط لمنع ظهور أخطاء 404
 });
